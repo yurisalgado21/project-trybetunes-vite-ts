6de5f307835cb3aca4fc02f502291d4083614ca4
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-07-19 13:20:16 -0300

    Exercício 4 feito

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Search from './components/Search';
 import Album from './components/Album';
+import Layout from './components/Layout';
+import NotFound from './components/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
-      <Route path="/search" element={ <Search /> } />
-      <Route path="/album/:id" element={ <Album /> } />
+      <Route path="" element={ <Layout /> }>
+        <Route path="/search" element={ <Search /> } />
+        <Route path="/album/:id" element={ <Album /> } />
+      </Route>
+      <Route path="*" element={ <NotFound /> } />
     </Routes>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { NavLink, useParams } from 'react-router-dom';
+import { getUser } from '../services/userAPI';
+import { UserType } from '../types';
+import LoadingMessage from './LoadingMessage';
+
+export default function Header() {
+  const [nameUser, setNameUser] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      setLoading(true);
+      const user = await getUser();
+      setNameUser(user.name);
+      setLoading(false);
+    };
+    fetchUser();
+  }, []);
+
+  return (
+    <header data-testid="header-component">
+      {loading && <LoadingMessage />}
+      <span data-testid="header-user-name">{nameUser}</span>
+      <nav>
+        <NavLink to="/search" data-testid="link-to-search">Search</NavLink>
+      </nav>
+      <nav>
+        <NavLink to="/favorites" data-testid="link-to-favorites">Favorites</NavLink>
+      </nav>
+      <nav>
+        <NavLink to="/profile" data-testid="link-to-profile">Profile</NavLink>
+      </nav>
+    </header>
+  );
+}
